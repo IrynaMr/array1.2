d3da22e7624ffa93287c67a397858287d4a3d2bb
IrynaMr <163152858+IrynaMr@users.noreply.github.com>
2024-04-04 18:18:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-        //2
-int array [] = new array[20];
-        for (int i = 0; i < array.length; i++) {
-            array[i]=(int)(Math.random()*10);
-        }
-        System.out.println("Початковий масив:");
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] + " ");
+         //3
+        int array1[] = new int[5];
+        int array2[] = new int[5];
+        int array3[] = new int[5];
+        double mid1 = 0, mid2 = 0, mid3 = 0;
+        for (int i = 0; i < 5; i++) {
+            array1[i] = (int) Math.round(Math.random() * 10);
+            mid1 = mid1 + array1[i];
+            System.out.print(array1[i] + " ");
         }
         System.out.println();
-        for (int i = 1; i < array.length; i+=2) {
-            array[i]=0;
+        for (int i = 0; i < 5; i++) {
+            array2[i] = (int) Math.round(Math.random() * 10);
+            mid2 = mid2 + array2[i];
+            System.out.print(array2[i] + " ");
         }
-        System.out.println("Оновлений  масив:");
+        for (int i = 0; i < 5; i++) {
+            array3[i] = (int) Math.round(Math.random() * 10);
+            mid3 = mid3 + array3[i];
+            System.out.println(array3[i] + " ");
+        }
+        System.out.println();
+        System.out.println(mid1 = mid1 / 5);
+        System.out.println(mid2 = mid2 / 5);
+        System.out.println(mid3 = mid3 / 5);
+        if (mid1 < mid2 && mid1 < mid3)
+            System.out.println("mid1<mid2 && mid1<mid3");
+        else if (mid1 > mid2 && mid1 > mid3)
+            System.out.println("mid1>mid2 && mid1>mid3");
+        else if (mid1 == mid2 && mid1 == mid3)
+            System.out.println("mid1==mid2 && mid1>mid3");
+
+        //6
+        int[][] array = new int[15][];
+        array[0] = new int[5];
+        array[1] = new int[5];
+        array[2] = new int[5];
+        array[3] = new int[5];
+        array[4] = new int[8];
+        array[5] = new int[8];
+        array[6] = new int[8];
+        array[7] = new int[8];
+        array[8] = new int[3];
+        array[9] = new int[3];
+        array[10] = new int[3];
+        array[11] = new int[3];
+        array[12] = new int[9];
+        array[13] = new int[9];
+        array[14] = new int[9];
+
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] +" ");
-        }
-}
- }
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 15);
+                System.out.print(array[i][j] + " ");
+            }
+    }
+        }}
+ 
